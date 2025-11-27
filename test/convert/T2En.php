0c165daaed154a4c5aeda8c3f6ee81cd1de5c8d8
Nigromante <nigromante@github.com>
2025-11-27 08:29:54 -0300

    test speak

--- test/convert/T2En.php
+++ test/convert/T2En.php
@@ -1,19 +1,36 @@
 <?php
-system("clear");
 require_once "../../vendor/autoload.php" ; 
 
 use Ez4us\Tools\convert\Number2Letter\Number2LetterEN;
 
 
-function test( $n ) {
-    $response = Number2LetterEN::GetText( $n ) ;
-    echo "\n\n $n :: " . $response . "\n\n" ;
+function clrscr() {
+  system("clear");
 }
 
-$line = fgets(STDIN);
-while( strtoupper(trim($line)) != "FIN") {
-  test( (int) $line ) ;
-  $line = fgets(STDIN);
+
+function say( $text ) {
+  echo  $text . "\n" ;
+  system("espeak  -s80 '". $text ."' & ") ;
 }
+
+
+function convert_number( $n ) {
+  $response = Number2LetterEN::GetText( $n ) ;
+  say($response) ;
+} 
+
+
+function work_loop() {
+  $line = fgets(STDIN);
+  while( strtoupper(trim($line)) != "END") {
+    convert_number( (int) $line ) ;
+    $line = fgets(STDIN);
+  }
+}
+
+
+clrscr();
+work_loop();
 
 
--- test/convert/T2Es.php
+++ test/convert/T2Es.php
@@ -1,19 +1,35 @@
 <?php
-system("clear");
 require_once "../../vendor/autoload.php" ; 
 
 use Ez4us\Tools\convert\Number2Letter\Number2LetterES;
 
 
-function test( $n ) {
-    $response = Number2LetterES::GetText( $n ) ;
-    echo "\n\n $n :: " . $response . "\n\n" ;
+function clrscr() {
+  system("clear");
 }
 
-$line = fgets(STDIN);
-while( strtoupper(trim($line)) != "FIN") {
-  test( (int) $line ) ;
+
+function say( $text ) {
+  echo  $text . "\n" ;
+  system("espeak -ves+f4 -s80 '". $text ."' & ") ;
+}
+
+
+function convert_number( $n ) {
+  $response = Number2LetterES::GetText( $n ) ;
+  say($response) ;
+} 
+
+
+function work_loop() {
   $line = fgets(STDIN);
+  while( strtoupper(trim($line)) != "FIN") {
+    convert_number( (int) $line ) ;
+    $line = fgets(STDIN);
+  }
 }
 
+
+clrscr();
+work_loop();
 
